904438a622b66e668aac50d78fc6a9bf76b08388
11aprilian <ihzaaprilian@gmail.com>
2023-04-05 22:07:14 +0700

    hadeuh

--- src/components/layouts/ListHistory.jsx
+++ src/components/layouts/ListHistory.jsx
@@ -46,9 +46,8 @@ const ListHistory = (props) => {
         <div className="card-body">
           <div className="d-flex justify-content-between">
             <div>
-              <a
-                href=""
-                className="stretched-link text-decoration-none fw-bold text-danger"
+              <button
+                className="stretched-link btn btn-link text-decoration-none fw-bold text-danger"
                 onMouseEnter={(e) => setOrderID(e.target.innerText)}
                 onClick={() => {
                   orderDetail();
@@ -59,7 +58,7 @@ const ListHistory = (props) => {
                     <Skeleton count={1} width={250} height={20} />
                   )}
                 </p>
-              </a>
+              </button>
               <small className="card-orderId">
                 {emulatorRute || <Skeleton count={1} width={100} height={20} />}
               </small>
